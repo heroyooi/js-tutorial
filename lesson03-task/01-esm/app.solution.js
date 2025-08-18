f41434a98c12da994e67562b1cdb287dff116d35
heroyooi <heroyooi1018@gmail.com>
2025-08-18 10:48:50 +0900

    3회차 – 모듈 시스템 (ES Modules vs CommonJS)

---
+++ lesson03-task/01-esm/app.solution.js
@@ -0,0 +1,6 @@
+import { add, sub, mul, div } from './math.js';
+
+console.log('덧셈:', add(2, 3)); // 5
+console.log('뺄셈:', sub(5, 2)); // 3
+console.log('곱셈:', mul(3, 4)); // 12
+console.log('나눗셈:', div(10, 2)); // 5
---
+++ lesson03-task/02-commonjs/app.solution.cjs
@@ -0,0 +1,6 @@
+const { add, sub, mul, div } = require('./math.cjs');
+
+console.log('덧셈:', add(2, 3)); // 5
+console.log('뺄셈:', sub(5, 2)); // 3
+console.log('곱셈:', mul(3, 4)); // 12
+console.log('나눗셈:', div(10, 2)); // 5
